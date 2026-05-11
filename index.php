314e01245c8c50e656ec72eac2a1ae96435c3b43
mounajrr <57331602+mounajrr@users.noreply.github.com>
2026-05-11 15:00:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-// Get 'name' parameter from URL, default to 'Guest'
-$name = isset($_GET['name']) ? htmlspecialchars($_GET['name']) : 'Guest';
-
-// Display greeting
 echo "<h1>Hello, $name!</h1>";
-
-// Simple form to enter name
-echo '<form method="get">
-        <input type="text" name="name" placeholder="Enter your name">
-        <input type="submit" value="Greet me">
-      </form>';
 ?>
